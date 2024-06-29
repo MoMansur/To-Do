3786647ab1d609e109f8c9cbd967bc1573f79dfd
MoMansur <mansurshehumsm@gmail.com>
2024-06-29 17:16:09 +0300

    config update, installed babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
 const path = require('path');
- const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-   mode: 'development',
-   entry: {
-     filename: './src/index.js',
-   },
-   output: {
-     filename: 'main.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    project: './src/project.js',
+  },
+  output: {
+    filename: '[name].bundle.js', // Use [name] placeholder for dynamic filenames
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
   devtool: 'inline-source-map',
-   plugins: [
-     new HtmlWebpackPlugin({
-       template: './src/index.html'
-     }),
-   ],
-   module: {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
     rules: [
       {
         test: /\.css$/i,
@@ -26,8 +27,20 @@ const path = require('path');
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[hash][ext][query]',
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
     ],
   },
-   
- };
+};
