4fec35f67884a3d4737c21545ab85fd9bf1985cc
MoMansur <mansurshehumsm@gmail.com>
2024-06-29 19:24:01 +0300

    Algortihtm update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
 // import { v4 as uuidv4 } from 'uuid';
+const container = document.getElementById('container')
 
 class Project{
     constructor(id, name){
         this.id = id
         this.name = name
-        this.project = []
-        
+        this.project = []   
     }  
-    pushNewTask(arr){
+    addTasks(arr){
         this.project.push(arr)
        return this.project
     }
     displayTask(){
         console.log(this.project)
+        return this.project
     }
 }
+class ProjectManager{
+    constructor(){
+        this.allProjectsArr = []
+        this.newProject = new Project()
+        this.defaultProject = new Project(1, 'Default')
+        this.allTask = []
+    }
+    newProjectFunc(id, name){
+        this.newProject = new Project(id, name)
+        return this.newProject
+    }
+    allProjects(){
+        this.allProjectsArr.push(this.newProject)
+        console.log(this.allProjectsArr)
+    }
+}
+const newProjectManager = new ProjectManager()
+
 
 class taskArray{
     constructor (title, description, dueDate, priority){
@@ -25,31 +44,26 @@ class taskArray{
     }
 }
 
-const newProject = new Project(2, 'Test')
 
+const newProject = newProjectManager.defaultProject
 
-function newProjectFunc(arr){
-    newProject.pushNewTask(arr)
-    newProject.displayTask()
-}
+console.log(newProject)
 
 function newTask(title, description, dueDate, priority) {
     const theNewTask = new taskArray(title, description, dueDate, priority);
-  
-    newProjectFunc(theNewTask)
+    
+    newProject.addTasks(theNewTask)
+    newProject.displayTask()
 }
 
  newTask(3,2,3,4)
  newTask(3,4,3,4)
  newTask(3,2,3,4)
  newTask(3,4,3,4)
+
  
 const testBtn = document.getElementById('testBtn')
 
-class ProjectManager{
-    constructor(){
-        this.list = []
-        this.allTask = []
-    }
-}
 
+
+newProjectManager.allProjects()
