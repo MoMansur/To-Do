c528c78b23d6c6f6189f2e8bef0f59cb24ee821b
MoMansur <mansurshehumsm@gmail.com>
2024-07-02 01:20:27 +0300

    delete function working on project page

--- src/projectManager.js
+++ src/projectManager.js
@@ -2,7 +2,6 @@ const taskSpace = document.getElementById('taskSpace')
 
 import Project from "./project.js"
 import modal from "./projectDom.js"
-import { displayTaskSpace } from "./index.js"
 import { loadProjectsFromLocalStorage, saveProjectsToLocalStorage } from './localStorage.js'
 import newProjectPopUpForm from "./taskForm.js"
 
@@ -36,23 +35,19 @@ export default class ProjectManager{
   }
 
   displayer() {  
-    this.allProjectFolder.forEach((project) => {
-     this.domCreator()
-    });
+    this.domCreator()
   }
 
   deleteProject(index) {
-    this.allProjectFolder = this.allProjectFolder.filter((_, i) => i !== index);
+    this.allProjectFolder.splice(index, 1);    
     this.saveProjects();
     this.displayer(); 
   }
   
   
   domCreator(){
-    this.liCreate(this.allProjectFolder);
+    this.projectPageandSideBarDOM(this.allProjectFolder);
     this.saveButton(this.allProjectFolder);
-    displayTaskSpace(this.allProjectFolder);
-
   }
 
   saveButton(project) {
@@ -63,16 +58,50 @@ export default class ProjectManager{
       const id = this.allProjectFolder.length + 1;
       
       this.addNewProject(id, name, color);
-      this.liCreate(project)
-      displayTaskSpace(this.allProjectFolder);   
+      this.projectPageandSideBarDOM(project)
       modal.closeModal();
     });
   }
-  
-  liCreate(projects) {
+
+    getSelectedProjectArray(array){
+      let selectArray = array
+      console.log(selectArray)
+      this.refresher()
+      Project.prototype.displayer(selectArray);
+      taskSpace.append(newProjectPopUpForm(selectArray).form)
+      this.saveProjects()
+      return selectArray
+    }
+
+    refresher(){
+      taskSpace.innerHTML = ""
+      this.displayer()
+    }
+    //DOM
+  projectPageandSideBarDOM(projects) {
     const newProjectUL = document.getElementById('newProjectUL');
     newProjectUL.innerHTML = ""
+    taskSpace.innerHTML = ""
 
+     // Create the main container for the task space
+     const homePage = document.createElement('div');
+     homePage.id = 'homePage';
+     homePage.style.padding = '20px';
+     homePage.style.border = '1px solid #ccc';
+     homePage.style.borderRadius = '5px';
+   
+     // Create and append the title
+     const title = document.createElement('h2');
+     title.innerText = 'My Todos Project';
+ 
+     
+     homePage.appendChild(title);
+   
+     // Create and append the list container
+     const projectList = document.createElement('ul');
+     projectList.style.listStyleType = 'none';
+     projectList.style.padding = '0';
+  
     projects.forEach((project, index) => {
     const listItem = document.createElement('li');
     listItem.className = 'list-group-item';
@@ -90,27 +119,49 @@ export default class ProjectManager{
     listItem.addEventListener('click', () => {
       let getProjectArray = project.project;
       this.getSelectedProjectArray(getProjectArray);
-    });
-    newProjectUL.append(listItem);
     })
-   }
-    
-    getSelectedProjectArray(array){
-      let selectArray = array
-      console.log(selectArray)
-      this.refresher()
-      Project.prototype.displayer(selectArray);
 
-      taskSpace.append(newProjectPopUpForm(selectArray).form)
+      const listItemTaskSpace = document.createElement('li');
+      listItemTaskSpace.className = 'project-item';
+      listItemTaskSpace.style.padding = '10px';
+      listItemTaskSpace.style.borderBottom = '1px solid #ccc';
+      listItemTaskSpace.innerText = project.name;
+      listItemTaskSpace.setAttribute('data-index', index);
+
+        const deleteIcon = document.createElement('i');
+        deleteIcon.className = 'fa-solid fa-trash';
+        deleteIcon.id = 'deleteIcon';
+        deleteIcon.style.float = 'right';
+        deleteIcon.style.cursor = 'pointer';
+
+        deleteIcon.addEventListener('click', (e) => {
+            e.stopPropagation();
+            console.log()
+          });
+
+        listItemTaskSpace.appendChild(deleteIcon);
+        projectList.appendChild(listItemTaskSpace);
+
+        listItemTaskSpace.addEventListener('click', () => {
+          let getProjectArray = project.project;
+          this.getSelectedProjectArray(getProjectArray);
+        })
+
+
+    deleteIcon.addEventListener('click', ()=>{
+      const index = parseInt(listItemTaskSpace.getAttribute("data-index"));
+      this.deleteProject(index)
       this.saveProjects()
-
-      return selectArray
-    }
-
+    });
+    
+    homePage.appendChild(projectList);
    
+    taskSpace.appendChild(homePage);
+    newProjectUL.append(listItem);
 
-    refresher(){
-      taskSpace.innerHTML = ""
-      this.displayer()
-    }
+    })
+
+   }
+
+ 
 }
